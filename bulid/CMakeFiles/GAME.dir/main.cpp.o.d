7b20596d7c098800ff89913d0cff22bd7f46fa68
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-04-24 19:02:18 +0200

    clean

--- bulid/CMakeFiles/GAME.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/GAME.dir/main.cpp.o: \
-  /Users/koxu2115/Desktop/kody/simple-game/main.cpp \
-  /Users/koxu2115/Desktop/kody/simple-game/bulid/_deps/raylib-src/src/raylib.h \
-  /Library/Developer/CommandLineTools/usr/lib/clang/15.0.0/include/stdarg.h
